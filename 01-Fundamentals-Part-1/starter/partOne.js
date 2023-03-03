05d6bde98d516212ab551cac715d56249893d0e6
Dawningale <53175097+Dawningale@users.noreply.github.com>
2023-03-03 13:08:19 +0000

    Fundamental1
    Starting fundamental 1 exersice.

---
+++ 01-Fundamentals-Part-1/starter/partOne.js
@@ -0,0 +1,21 @@
+//CONSTANTS
+const COUNTRY = "Germany";
+const CONTINENT = "Europe";
+const LANGUAGE = "German";
+const IS_ILAND = false;
+
+/*
+Country, continent, language and is_island are constants because they won't change in unforeseen future or perhaps never.
+
+However, the population of a country is alsways changing.
+*/
+
+//Variables
+let population = 84270625;
+
+//CONSOLE OUTPUT
+console.log("Country name: "+COUNTRY);
+console.log("Continent name: "+CONTINENT);
+console.log("Language: "+ LANGUAGE);
+console.log("Is an island: "+ IS_ILAND);
+console.log("The population devided by 2 is: "+(population/2));
