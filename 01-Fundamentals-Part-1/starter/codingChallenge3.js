fd5be646fa05971f5233bffed28fd9e1302cd74b
Dawningale <53175097+Dawningale@users.noreply.github.com>
2023-03-06 16:16:20 +0000

    literal syntax
    coding challenge 3:
    Adding literal syntax in the if/else-statements

--- 01-Fundamentals-Part-1/starter/codingChallenge3.js
+++ 01-Fundamentals-Part-1/starter/codingChallenge3.js
@@ -1,14 +1,15 @@
-const avScoreDolphin = (97+112+101) / 3;
-const avScoreKoalas = (109+95+106) / 3;
+const avScoreDolphin = (97 + 112 + 101) / 3;
+const avScoreKoalas = (109 + 95 + 106) / 3;
+let decision;
 
-console.log(avScoreDolphin, avScoreKoalas);
-
-if(avScoreDolphin > avScoreKoalas && avScoreDolphin >= 100){
-	console.log("Dolphins won with an average score of "+avScoreDolphin+"!");
-}else if(avScoreDolphin < avScoreKoalas && avScoreKoalas >= 100){
-	console.log("Koalas won with an average score of "+avScoreKoalas+"!");
-}else if(avScoreDolphin == avScoreKoalas && avScoreDolphin >= 100 && avScoreKoalas >= 100){
-	console.log("It's a tie!");
-}else{
-	console.log("Neither Dolphins nor Koalas have won!");
+if (avScoreDolphin > avScoreKoalas && avScoreDolphin >= 100) {
+	decision = `Dolphins won with an average score of $(avScoreDolphin)!`;
+} else if (avScoreDolphin < avScoreKoalas && avScoreKoalas >= 100) {
+	decision = `Koalas won with an average score of $(avScoreKoalas)!`;
+} else if (avScoreDolphin == avScoreKoalas && avScoreDolphin >= 100 && avScoreKoalas >= 100) {
+	decision = `It's a tie!`;
+} else {
+	decision = `Neither Dolphins nor Koalas have won!`;
 }
+
+console.log(decision);
