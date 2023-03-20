7cfdbac37eae15ee24686eb7f76e5b195f404699
Dawningale <53175097+Dawningale@users.noreply.github.com>
2023-03-20 14:35:40 +0000

    p2cc3update

--- 02-Fundamentals-Part-2/starter/codingChallenge3.js
+++ 02-Fundamentals-Part-2/starter/codingChallenge3.js
@@ -1,12 +1,25 @@
 //BMI Calculator
 
-const person = {
+//OBJECTS
+const person1 = {
     name: "John Smith",
     mass: 92,
     height: 1.95,
     bmi: function () {
-        return (this.mass / this.height ** 2);
+        return (Math.round(this.mass / this.height ** 2));
     }
 };
 
-console.log(person.bmi());
+const person2 = {
+    name: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    bmi: function () {
+        return (Math.round(this.mass / this.height ** 2));
+    }
+};
+
+//VARIABLES
+const out = `${person1.name} (${person1.height}m,${person1.mass}kg,BMI of ${person1.bmi()}) has a higher BMI than ${person2.name} (${person2.height}m,${person2.mass}kg,BMI of ${person2.bmi()}): ${person1.bmi() > person2.bmi()}`;
+
+console.log(out);
